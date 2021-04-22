a590e46ee0a02cbe855fe1c774c04a0870645a16
Roman Paolucci <romanmichaelpaolucci@gmail.com>
2021-04-22 09:48:01 -0400

    Simulations

--- QFin/__init__.py
+++ QFin/__init__.py
@@ -1,5 +1,6 @@
-import tvm.*
-import bonds.*
-import options.*
-import simulations.*
-import futures.*
+from qfin.options import BlackScholesCall
+from qfin.options import BlackScholesPut
+from qfin.simulations import MonteCarloBinaryCall
+from qfin.simulations import MonteCarloBinaryPut
+from qfin.simulations import MonteCarloCall
+from qfin.simulations import MonteCarloPut
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = '0.0.11'
+VERSION = '0.0.14'
 DESCRIPTION = 'A Python package for mathematical finance.'
 
 # Setting up
