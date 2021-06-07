570e7322518cb57187c77cf173f7e013123b88ab
Roman Paolucci <romanmichaelpaolucci@gmail.com>
2021-06-07 12:23:33 -0400

    Updates

--- QFin/simulations.py
+++ QFin/simulations.py
@@ -12,7 +12,10 @@ class GeometricBrownianMotion:
             ds = prev_price*mu*dt + prev_price*sigma*np.random.randn()*np.sqrt(dt)
             prev_price = prev_price+ds
             prices.append((prev_price))
-            step += dt
+            if step + dt > T:
+                dt = T-step
+            else:
+                step += dt
         return prices
 
     def __init__(self, S, mu, sigma, dt, T):
@@ -30,7 +33,6 @@ class StochasticVarianceModel:
         while step < T:
             e1 = norm.ppf(np.random.random())
             e2 = e1*rho + np.sqrt(1-(rho**2))*norm.ppf(np.random.random())
-
             price_now = price_now + (r - div) * price_now * dt + price_now * np.sqrt(prev_inst_var * dt) * e1
             prev_inst_var = inst_var_now
             inst_var_now = prev_inst_var + alpha*(beta - prev_inst_var)*dt + vol_var*np.sqrt(prev_inst_var*dt)*e2
@@ -41,7 +43,10 @@ class StochasticVarianceModel:
             else:
                 inst_var_now = .0000001
             prices.append(price_now)
-            step += dt
+            if step + dt > T:
+                dt = T-step
+            else:
+                step += dt
         return prices
 
     def __init__(self, S, mu, r, div, alpha, beta, rho, vol_var, inst_var, dt, T):
